7786ceb4b9d5ce42f7988e3fc10140a359cb43bb
ZayarPhyo4774 <zayarutycc4499@gmail.com>
2026-09-08 22:26:40 +0630

    fix deploy

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-
-# Ensure build fails on any command error
-set -e
-
-echo "=== Installing Dependencies ==="
-python3 -m pip install -r requirements.txt
-
-echo "=== Collecting Static Files ==="
-python3 manage.py collectstatic --noinput --clear
-
-echo "=== Build Complete ==="
+python -m pip install --break-system-packages -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
